a89587e042a42fface1cee71de1baa5aa814c163
Salvatore Criniti <salvatore.criniti96@gmail.com>
2024-11-17 14:52:15 +0100

    refactor(types): update request config interface and remove unused import
    - Remove unused AxiosInstance import
    - Add AxcacheRequestConfig interface extending AxiosRequestConfig
    - Update HTTP methods to use AxcacheRequestConfig for better type safety

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,4 +1,4 @@
-import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export interface AxcacheOptions extends AxiosRequestConfig {
   stdTTL?: number;
@@ -8,12 +8,17 @@ export interface AxcacheOptions extends AxiosRequestConfig {
   onCacheWrite?: (key: string) => void;
 }
 
+export interface AxcacheRequestConfig extends AxiosRequestConfig {
+  forceRefresh?: boolean;
+  ttl?: number;
+}
+
 export interface Axcache {
-  get<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
-  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
-  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
-  delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
-  patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  get<T = any>(url: string, config?: AxcacheRequestConfig): Promise<AxiosResponse<T>>;
+  post<T = any>(url: string, data?: any, config?: AxcacheRequestConfig): Promise<AxiosResponse<T>>;
+  put<T = any>(url: string, data?: any, config?: AxcacheRequestConfig): Promise<AxiosResponse<T>>;
+  delete<T = any>(url: string, config?: AxcacheRequestConfig): Promise<AxiosResponse<T>>;
+  patch<T = any>(url: string, data?: any, config?: AxcacheRequestConfig): Promise<AxiosResponse<T>>;
   getCacheStats(): { entries: number; size: number };
   clearCache(): void;
 }
